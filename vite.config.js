86126fbbbbd63c4cdd543a7a163ea12ee017fb5d
pawansai-code <pawansairdj17@gmail.com>
2025-12-29 23:42:49 +0530

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -37,4 +38,5 @@ export default defineConfig({
       }
     })
   ],
+   base: process.env.VITE_BASE_PATH || "/artgallery"
 });
